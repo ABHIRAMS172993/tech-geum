54dad4633b17835e5a7ca0dc8ed0abc674660213
Abhiram <abhi172993@gmail.com>
2026-09-09 20:26:47 +0530

    .env

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0p7@15nz)$rco^s^08easj1c7ipo8mjg)j1-9u=3r1%zt=!_o='
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
